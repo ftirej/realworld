4002c2e0725ade5963d0bb9571a281d0c0c3f644
Fernando Tirelli <fernando@Fernandos-MacBook-Pro.local>
2020-01-22 11:43:32 -0300

    Add enzyme Move test library to devDependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+
 import React from "react";
-import ReactDOM from "react-dom";
+import { render } from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
@@ -14,7 +17,7 @@ syncTranslationWithStore(store);
 store.dispatch(loadTranslations(translations));
 store.dispatch(setLocale("en"));
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     <App />
   </Provider>,
--- src/store/common/i18n/reducer.test.js
+++ src/store/common/i18n/reducer.test.js
@@ -1,39 +1,39 @@
-import { I18n } from 'react-i18nify'
-import initialState from './initialState'
-import reducer from './reducer'
-import * as actions from './actions'
-import * as actionTypes from './actionTypes'
+import { t } from "react-i18nify";
+import initialState from "./initialState";
+import reducer from "./reducer";
+import * as actions from "./actions";
+import * as actionTypes from "./actionTypes";
 
-describe('i18n Reducer', () => {
-  describe('i18n Reducer - Load Translations', () => {
+describe("i18n Reducer", () => {
+  describe("i18n Reducer - Load Translations", () => {
     it(`should work when passed ${actionTypes.I18N_LOAD_TRANSLATIONS}`, () => {
       // Arrange.
       const translations = {
-        'en': {
-          'test': {
-            'value1': 'Value1'
+        en: {
+          test: {
+            value1: "Value1"
           }
         }
-      }
+      };
 
-      actions.loadTranslations(translations)
+      actions.loadTranslations(translations);
 
       // Assert.
-      expect(I18n.t('test.value1')).toEqual('Value1')
-    })
-  })
+      expect(t("test.value1")).toEqual("value1");
+    });
+  });
 
-  describe('i18n Reducer - Set Locale', () => {
+  describe("i18n Reducer - Set Locale", () => {
     it(`should work when passed ${actionTypes.I18N_SET_LOCALE}`, () => {
       // Arrange.
-      const language = 'en'
-      const action = actions.setLocale(language)
+      const language = "en";
+      const action = actions.setLocale(language);
 
       // Act.
-      const newState = reducer(initialState, action)
+      const newState = reducer(initialState, action);
 
       // Assert.
-      expect(newState).not.toBeNull()
-    })
-  })
-})
+      expect(newState).not.toBeNull();
+    });
+  });
+});
